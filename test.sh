0af783ac75ca0ce286f18e065fb02ce857d1944d
Steffen (Daode) Nurpmeso <steffen@sdaoden.eu>
2016-10-17 20:01:57 +0200

    Reimplement MarkLo, support \} to be embedded

--- test.sh
+++ test.sh
@@ -1353,19 +1353,19 @@ Hello, Honey   Sugar Candy	Girl
 ''
 
 ## }}}
-
 ## MarkLo {{{
 
-printf '<?begin?>START\\c{tt}\\i{em}\\b{strong}\\u{u}\\i{I \\b{really \\u{love} you}, baby!}END\n\\a{xname}\\l{http://plan9.bell-labs.com/plan9/}<?end?>\n' > ./test-0042-w42-ats
+printf '<?begin?>START\\c{t{e\\}st}\\c{tt}\\i{em}\\b{strong}\\u{u}\\i{I \\b{really \\u{{love\\}} you}, baby!}END\n\\a{xname}\\l{http://plan9.bell-labs.com/plan9/}\\b{OPEN-ERROR<?end?>\n' > ./test-0042-w42-ats
 
 tcase 'MarkLo expansion (disable mode: m)' 0042-w42-ats \
 '' \
 \
-'START<tt>tt</tt><em>em</em><strong>strong</strong><u>u</u><em>I <strong>really <u>love</u> you</strong>, baby!</em>END
+'START<tt>t{e}st</tt><tt>tt</tt><em>em</em><strong>strong</strong><u>u</u><em>I <strong>really <u>{love}</u> you</strong>, baby!</em>END
 <a name="xname"></a><a href="http://plan9.bell-labs.com/plan9/">http://plan9.bell-labs.com/plan9/</a>
 ' \
 \
-''
+"ERROR 'test-0042-w42-ats':2: MarkLo: \\\\b not closed (missing right brace)
+"
 
 ## }}}
 ## Whitespace normalization {{{
